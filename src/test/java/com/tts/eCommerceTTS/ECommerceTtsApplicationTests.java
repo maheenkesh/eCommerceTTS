cbce269f5680cdcc9fe9244652534dd5dfb5bdb6
maheen k <maheenk@maheens-MacBook-Pro-2.local>
2020-07-07 09:52:12 -0700

    initial commit

---
+++ src/test/java/com/tts/eCommerceTTS/ECommerceTtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.eCommerceTTS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceTtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
