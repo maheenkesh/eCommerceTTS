67a77369b3d41054e3e6911c5a5b03bf52576232
msada44 <mollysada2016@yahoo.com>
2020-07-08 10:01:53 -0500

    add role model

---
+++ src/main/java/com/tts/eCommerceTTS/model/Role.java
@@ -0,0 +1,34 @@
+package com.tts.eCommerceTTS.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
+public class Role {
+	@Id 
+	@GeneratedValue(strategy = GenerationType.AUTO) 
+	@Column(name = "role_id") 
+	
+	private Long id; 
+	private String role;
+	public Role() {
+		
+	}
+	public Role(Long id, String role) {
+		this.id = id;
+		this.role = role;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public Long getId() {
+		return id;
+	} 
+	
+}
